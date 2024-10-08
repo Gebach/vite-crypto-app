30d20578a481df1413083b9908e227720227c8bb
Rodion <swizi_hs@mail.ru>
2024-10-08 16:46:45 +0500

    tailwind initialized

--- src/pages/coins/CoinPage.jsx
+++ src/pages/coins/CoinPage.jsx
@@ -31,9 +31,13 @@ export default function CoinPage() {
   return isLoading ? (
     <Spin fullscreen />
   ) : (
-    <div>
-      <h2>Coin {coin.id}</h2>
-      <p>Price: {coin.price.toFixed(2)}$</p>
+    <div className="group">
+      <h2 className="text-xl text-center hover:text-blue-600 transition-all duration-300 ease-in-out cursor-pointer">
+        Coin {coin.id}
+      </h2>
+      <p className="scale-0 group-hover:scale-100 text-center transition-all duration-200 ease-out">
+        Price: {coin.price.toFixed(2)}$
+      </p>
     </div>
   )
 }
